3a01c413d27c93506f30994dbb5c227635f53916
rizkifebriansyach-ibs <rizki.febriansyach@ib-synergy.co.id>
2025-03-17 15:13:22 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json()) // to parse the incoming request with JSON payloads (from req.body)
@@ -22,10 +25,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-
-// app.get("/", (req, res) => {
-//     res.send("Hello Rizki Febriansyach")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 server.listen(PORT, () => {
     connectToMongoDB()
